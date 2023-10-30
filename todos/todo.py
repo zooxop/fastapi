c3c27bfa69df65c122b899519adfc54f4c30c994
Cheolhyun Mun <zooxop@gmail.com>
2023-10-31 01:13:35 +0900

    add: Path class example

--- todos/todo.py
+++ todos/todo.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path
 from model import Todo
 
 todo_router = APIRouter()
@@ -16,4 +16,16 @@ async def add_todo(todo: Todo) -> dict:
 async def retrieve_todos() -> dict:
     return {  
         "todos": todo_list
+    }
+
+@todo_router.get("/todo/{todo_id}")
+async def get_single_todo(todo_id: int = Path(..., title="The ID of the todo to retrive.")) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+            return { 
+                "todo": todo
+            }
+    
+    return {
+        "message": "Todo with supplied ID doesn't exist."
     }
